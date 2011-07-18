05a933d5ecaeabfc2aa34a0bb08ad91b986707dc
Rob Sanchez <rsanchez@barrettnewton.com>
2011-07-17 22:19:57 -0500

    updated nsm addon updater url

--- system/expressionengine/third_party/github_addon_installer/config.php
+++ system/expressionengine/third_party/github_addon_installer/config.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['name'] = 'GitHub Addon Installer';
-$config['version'] = '1.0.0';
-$config['nsm_addon_updater']['versions_xml'] = 'http://rsanchez.github.com/nsm_addon_updater/github_addon_updater.xml';
+$config['version'] = '0.9.9';
+$config['nsm_addon_updater']['versions_xml'] = 'http://rsanchez.github.com/nsm_addon_updater/github_addon_installer.xml';
